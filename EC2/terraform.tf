769834e0bc4a424de9af3c3e702cc3841c1b75f8
Abishekgautam44 <abishekgautam44@gmail.com>
2025-07-31 12:41:56 +0545

    added: module vpc

--- EC2/terraform.tf
+++ EC2/terraform.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.91.0"
+      version = ">=6.0.0"
     }
   }
 
-  backend "s3" {
-    bucket = "abis-buckett"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "abis-db-table"
-  }
+  # backend "s3" {
+  #   bucket = "abis-buckett"
+  #   key = "terraform.tfstate"
+  #   region = "us-east-1"
+  #   dynamodb_table = "abis-db-table"
+  # }
 }
